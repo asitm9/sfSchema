9d444dab060281293ed7917ec858392771076052
asitm9 <asitm9@gmail.com>
2018-05-29 18:37:28 +0530

    field mapping added

--- base.py
+++ base.py
@@ -1,6 +1,7 @@
 from simple_salesforce import Salesforce
 from simple_salesforce import SFType
 import local
+import field_mapping as fm
 import json
 
 __author__ = 'asitm9'
@@ -9,7 +10,28 @@ if __name__ == '__main__':
     sf = Salesforce(username=local.username, password=local.password,
                     security_token=local.security_token)
     # print sf.Contact.describe()
-    SFType('Account', sf.session_id,
+    acoount = SFType('Account', sf.session_id,
            sf.sf_instance, sf_version=sf.sf_version,
            proxies=sf.proxies, session=sf.session).describe()
+    ds = sf.describe()
+    print sf.__dict__
+    f_types = set()
+    # for so in ds.get('sobjects'):
+    #     if so.get('createable') is True:
+    #         # print so.get('name')
+    #         so = SFType(so.get('name'),
+    #                     sf.session_id,
+    #                     sf.sf_instance, sf_version=sf.sf_version,
+    #                     proxies=sf.proxies, session=sf.session
+    #                     ).describe()
+    #         for fld in so.get('fields'):
+    #             print '{}\n'.format(fld.get('name'))
+
+    # for fld in acoount.get('fields'):
+    #     print 'name -> %s type -> %s' % (fld.get('name'), fld.get('soapType'))
+    obj_name = 'Account'
+    print 'class {}(models.Model):\n'.format(obj_name)
+
+    for s in acoount.get('fields'):
+        print '\t {} = models.{}()\n'.format(s.get('name'), fm.sf2django.get(s.get('soapType')))
 
---
+++ field_mapping.py
@@ -0,0 +1,13 @@
+
+__author__ = 'asitm9'
+
+sf2django = {
+    'tns:ID': 'CharField',
+    'xsd:time': 'TimeField',
+    'xsd:int': 'BigIntegerField',
+    'xsd:date': 'DateField',
+    'xsd:string': 'CharField',
+    'xsd:boolean': 'BooleanField',
+    'xsd:dateTime': 'DateTimeField',
+    'xsd:double': 'FloatField'
+}
